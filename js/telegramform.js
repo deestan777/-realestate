bc5eab7d7b0fc351bb8d8f361d8dbde40fa89d32
deestan <deestan@ymail.com>
2022-08-26 13:05:21 +0300

    update

---
+++ js/telegramform.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+  $(".form-element").submit(function () {
+    var formID = $(this).attr('id');
+    var formNm = $('#' + formID);
+    var message = $(formNm).find(".form-message");
+    var formTitle = $(formNm).find(".form-title");
+    $.ajax({
+      type: "POST",
+      url: './php/send-message-to-telegram.php',
+      data: formNm.serialize(),
+      success: function (data) {
+        // Вывод сообщения об успешной отправке
+        message.html(data);
+        formTitle.css("display", "none");
+        setTimeout(function () {
+          formTitle.css("display", "block");
+          message.html('');
+          $('input').not(':input[type=submit], :input[type=hidden]').val('');
+        }, 3000);
+      },
+      error: function (jqXHR, text, error) {
+        // Вывод сообщения об ошибке отправки
+        message.html(error);
+        formTitle.css("display", "none");
+        setTimeout(function () {
+          formTitle.css("display", "block");
+          message.html('');
+          $('input').not(':input[type=submit], :input[type=hidden]').val('');
+        }, 3000);
+      }
+    });
+    return false;
+  });
+
+
+});
+
+var fewSeconds = 5;
+$('#btn').click(function(){
+    // Ajax request
+    var btn = $(this);
+    btn.prop('disabled', true);
+    setTimeout(function(){
+        btn.prop('disabled', false);
+    }, fewSeconds*1000);
+});
